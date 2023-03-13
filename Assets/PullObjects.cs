57739bae3c0a73f602c874db5d0d8f3113709939
georgegausden <george.philip.gausden@gmail.com>
2023-03-13 14:18:19 -0400

    Prototype 4

---
+++ Assets/PullObjects.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PullObjects : MonoBehaviour
+{
+    public float pullForce = 10f;
+    public float maxDistance = 5f;
+
+    void Update()
+    {
+        // Check for all pullable objects within the max distance
+        GameObject[] pullableObjects = GameObject.FindGameObjectsWithTag("Actors");
+        foreach (GameObject obj in pullableObjects)
+        {
+            float distance = Vector3.Distance(obj.transform.position, transform.position);
+
+            if (distance <= maxDistance)
+            {
+                // Calculate the force based on the distance
+                float force = pullForce * (1f - distance / maxDistance);
+
+                // Apply the force towards the player
+                Vector3 direction = transform.position - obj.transform.position;
+                obj.GetComponent<Rigidbody>().AddForce(direction.normalized * force);
+            }
+        }
+    }
+}
--- Assets/RegenerateScene.cs
+++ Assets/RegenerateScene.cs
@@ -39,14 +39,42 @@ public class RegenerateScene : MonoBehaviour
             // Add a RotateAround component to the planet object and get its reference
             RotateAround rotateAround = planet.AddComponent<RotateAround>();
 
+            // Set the target for the RotateAround component to the sun
+            rotateAround.target = sun;
+
             // Set its speed to a random value between 0 and 5
             rotateAround.speed = Random.Range(0f, 5f);
 
             // Add a SelfRotate component to the planet object
             planet.AddComponent<SelfRotate>();
         }
-    }
 
+        // Add 5 asteroids to the scene
+        int numAsteroids = 5;
+        float asteroidRadius = 100f;
+        for (int i = 0; i < numAsteroids; i++)
+        {
+            // Instantiate the asteroid prefab at a random position on a circle with the asteroid radius
+            float angle = Random.Range(0f, 360f);
+            Vector3 position = new Vector3(Mathf.Cos(angle), 0f, Mathf.Sin(angle)) * asteroidRadius;
+            GameObject asteroid = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            asteroid.transform.position = position;
+
+            // Set its localScale to a random value between 1 and 5 for each axis
+            Vector3 scale3 = Vector3.one * Random.Range(1f, 5f);
+            asteroid.transform.localScale = scale3;
+
+            // Make it a child of the solar system object
+            asteroid.transform.parent = solarSystem.transform;
+
+            // Add the "pullable" tag to the asteroid object
+            asteroid.tag = "Actors";
+
+            // Add a Rigidbody component to the asteroid object
+            Rigidbody rb = asteroid.AddComponent<Rigidbody>();
+            rb.useGravity = false;
+        }
+    }
 
     private void Update()
     {
